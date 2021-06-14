097f110ae361d3287b9f5228b2850f486c012975
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-14 00:48:33 -0600

    Add output for see in view the data

--- Covid19Statistics.Web/Controllers/HomeController.cs
+++ Covid19Statistics.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Covid19Statistics.Dto.Stadistics;
+﻿using Covid19Statistics.Dto.Region;
+using Covid19Statistics.Dto.Stadistics;
 using Covid19Statistics.Service;
 using Covid19Statistics.Web.Models;
+using Covid19Statistics.Web.Models.Report;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -16,18 +18,60 @@ namespace Covid19Statistics.Web.Controllers
         private readonly ILogger<HomeController> _logger;
 
         private readonly ICovid19StatisticsService _covid19StatisticsService;
+        private readonly IRegionsService _regionsService;
 
-        public HomeController(ILogger<HomeController> logger, ICovid19StatisticsService covid19StatisticsService)
+        public HomeController(
+            ICovid19StatisticsService covid19StatisticsService,
+            IRegionsService regionsService)
         {
-            _logger = logger;
             _covid19StatisticsService = covid19StatisticsService;
+            _regionsService = regionsService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string Iso)
         {
+            ReportDetailOutput reportDetailOutput = new ReportDetailOutput()
+            {
+                 RegionIso= Iso
+            };
 
-            List<RegionStadisticsOutputDto> lst=await  _covid19StatisticsService.GetTopCasesCovidByRegions();
-            return View();
+            List<RegionOutputDto>  listRegionOutputDto = await _regionsService.GetRegionsAsync();
+            List<RegionOutput> regionOutputs = listRegionOutputDto.Select(z => new RegionOutput()
+            {
+                 Iso=z.Code,
+                 Name= z.Name
+
+            }).ToList();
+
+            reportDetailOutput.ListRegion = regionOutputs;
+
+            if (string.IsNullOrEmpty(Iso))
+            {
+                List<RegionStadisticsOutputDto> regionStadistics = await _covid19StatisticsService.GetTopCasesCovidByRegions();
+
+                reportDetailOutput.ReportDataOutput= regionStadistics.Select(z => new ReportDataOutput()
+                {
+                     Name= z.Name,
+                     Cases= z.Confirmed,
+                     Deaths= z.Deaths
+                }).ToList();
+            }
+
+            if (string.IsNullOrEmpty(Iso)==false)
+            {
+                List<ProvincesStadisticsOutputDto> provincesStadisticsOutputs = 
+                    await _covid19StatisticsService.GetTopCasesCovidFilter(new FilterCovid19Statistics(){ 
+                       RegionIsoCode= Iso
+                });
+                reportDetailOutput.ReportDataOutput = provincesStadisticsOutputs.Select(z => new ReportDataOutput()
+                {
+                    Name = z.Name,
+                    Cases = z.Confirmed,
+                    Deaths = z.Deaths
+                }).ToList();
+            }
+
+            return View(reportDetailOutput);
         }
 
         public IActionResult Privacy()
---
+++ Covid19Statistics.Web/Models/Report/RegionOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Statistics.Web.Models.Report
+{
+    public class RegionOutput
+    {
+        /// <summary>
+        /// Code Iso of the regios
+        /// </summary>
+        public string Iso { get; set; }
+        /// <summary>
+        /// Name of region
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
