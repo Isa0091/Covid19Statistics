3afa9accf173ba23abca4d006d5cccdaa9b4edcf
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-14 01:44:33 -0600

    Previo data to dowload in diferent type

--- Covid19Statistics.Web/Controllers/HomeController.cs
+++ Covid19Statistics.Web/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using Covid19Statistics.Dto.Stadistics;
 using Covid19Statistics.Service;
 using Covid19Statistics.Web.Models;
 using Covid19Statistics.Web.Models.Report;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Covid19Statistics.Web.Controllers
 {
@@ -71,6 +78,15 @@ namespace Covid19Statistics.Web.Controllers
                 }).ToList();
             }
 
+
+            string data = SerializeXmlRequest<List<ReportDataOutput>>(reportDetailOutput.ReportDataOutput);
+            var result = System.Text.Encoding.Unicode.GetBytes(data);
+
+            return File(result, "application/xml", $"export_{DateTime.UtcNow.Ticks}.xml");
+
+            byte[] data2= SerializeCsv(reportDetailOutput.ReportDataOutput);
+            return File(data2, "text/csv", $"export_{DateTime.UtcNow.Ticks}.csv");
+
             return View(reportDetailOutput);
         }
 
@@ -84,5 +100,61 @@ namespace Covid19Statistics.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /// <summary>
+        /// Serializa una clase a xml
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string SerializeXmlRequest<T>(T data)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(data.GetType());
+            var xmlrequest = "";
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, data);
+                    xmlrequest = sww.ToString();
+                }
+
+            }
+
+            return xmlrequest;
+        }
+
+
+        /// <summary>
+        /// Serializa una clase a xml
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string SerializeJson<T>(T data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        /// <summary>
+        /// Serializa una clase a xml
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private byte[] SerializeCsv<T>(IEnumerable<T> data)
+        {
+            var cc = new CsvConfiguration(new System.Globalization.CultureInfo("en-US"));
+            using (var ms = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(stream: ms, encoding: new UTF8Encoding(true)))
+                {
+                    using (var cw = new CsvWriter(sw, cc))
+                    {
+                        cw.WriteRecords(data);
+                    }// The stream gets flushed here.
+                }
+                return ms.ToArray();
+            }
+
+      }
     }
 }
