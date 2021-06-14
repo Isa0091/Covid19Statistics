ad258bacca228bb34ce2cea5e95a8fd30c09305c
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-13 22:11:27 -0600

    Service and repo of regions

---
+++ Covid19Statistics/Service/IRegionsService.cs
@@ -0,0 +1,20 @@
+﻿using Covid19Statistics.Dto.Region;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Statistics.Service
+{
+    /// <summary>
+    /// Data of reions
+    /// </summary>
+    public interface IRegionsService
+    {
+        /// <summary>
+        /// Get a list of regions
+        /// </summary>
+        /// <returns></returns>
+        Task<List<RegionOutputDto>> GetRegionsAsync();
+    }
+}
