b55ce79659ae9e4565e5109836690547ac50d4e6
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-15 01:00:03 -0600

    Create provider to convert to diferent format the data

---
+++ Covid19Statistics.Providers/XmlConvertProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Covid19Statistics.Providers
+{
+    public class XmlConvertProvider : IXmlConvertProvider
+    {
+        public byte[] ConvertToXml<T>(T data)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(data.GetType());
+            var xmlrequest = "";
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, data);
+                    xmlrequest = sww.ToString();
+                }
+
+            }
+
+            if(string.IsNullOrEmpty(xmlrequest))
+                return  System.Text.Encoding.Unicode.GetBytes(xmlrequest);
+
+            return null;
+        }
+
+    }
+}
---
+++ Covid19Statistics/Providers/IXmlConvertProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Statistics.Providers
+{
+    public interface IXmlConvertProvider
+    {
+        /// <summary>
+        /// Conver the data send in format xmls and reutn the bytes
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        byte[] ConvertToXml<T>(T data);
+    }
+}
